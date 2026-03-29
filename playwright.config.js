ff0eb07a718e3f37d2faf5a381435a2e8ac44c93
Ihar Sakun <Ihar_Sakun@epam.com>
2026-03-30 00:57:26 +0300

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "tests-e2e",
+  testDir: "./tests-e2e",
   timeout: 30_000,
   use: {
     baseURL: "http://localhost:5173", //наш Vite фронтенд
